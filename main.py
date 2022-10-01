a25afbd2f4284c7602dee2dee954af2baffe1337
EdwinEstep <edwin.estep@gmail.com>
2022-10-01 16:45:26 -0500

    Remove threading from main.py. Remove pycaches from repo.

--- main.py
+++ main.py
@@ -11,10 +11,8 @@ from src.gui.run import run_gui
 
 
 if __name__ == "__main__":
-    gui_thread = Thread(target=run_gui)
-    gui_thread.start()
 
-    # other processing here
-    request_juno_cam_img("https://www.missionjuno.swri.edu/junocam/processing?id=13926")
+    # data init
 
-    gui_thread.join()
+    
+    run_gui()
