7ea10716c3016d50e9c4dad482078953a39f16ba
Sam LaGrave <srl0011@uah.edu>
2022-10-01 16:33:16 -0500

    Main using proper loop & downloaded files

--- main.py
+++ main.py
@@ -6,15 +6,17 @@ from PIL import Image
 
 from src import *
 
-request_juno_cam_img("https://www.missionjuno.swri.edu/junocam/processing?id=13926")
+d = request_juno_cam_img("https://www.missionjuno.swri.edu/junocam/processing?id=13926")
 
 # Loop looks like this
 # Load image
-im = Image.open("pixmap.png")
+im = Image.open(d["thumbnail"])
 # Run before
 im = run_custom_before("./test.py", im)
 # Run any filters
-im = im.filter(filters[0]())
+
+
+
 # Run after
 im = run_custom_after("./test.py", im)
 # Show it
